e5d50aa12d2694b8e40291821c9464b95ff6f2a1
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2023-05-10 15:58:51 +0800

    Initial commit

---
+++ app/admin/pages/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versi</b> 1.0
+    </div>
+    <strong>Hak Cipta &copy; <?= date('Y'); ?> SMA Kristen Kanaan </strong> Build By <a href='https://github.com/tianreformis/' title='Kristian Reformis' target='_blank'>Kristian Reformis</a>
+        
+</footer>
